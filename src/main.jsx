c1c1ae2f91653d9e12a64208a6c552386a6da0c2
Saiful264 <saifulislamx264@gmail.com>
2023-06-09 22:55:57 +0600

    helmet added for dynamic title

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,16 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes.jsx";
 import { Toaster } from "react-hot-toast";
 import AuthProvider from "./providers/AuthProvider";
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from "react-helmet-async";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <HelmetProvider>
+    <HelmetProvider>
       <RouterProvider router={router} />
       <Toaster />
-      </HelmetProvider>
+    </HelmetProvider>
     </AuthProvider>
   </React.StrictMode>
 );
--- src/page/Shared/LogIn/Login.jsx
+++ src/page/Shared/LogIn/Login.jsx
@@ -2,16 +2,16 @@ import { Form, Link, useLocation, useNavigate } from "react-router-dom";
 import login from "../../../assets/login.jpg";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 import { useForm } from "react-hook-form";
-import { Helmet } from "react-helmet-async";
 import useAuth from "../../../hooks/useAuth";
 import Swal from "sweetalert2";
 import { toast } from "react-hot-toast";
+import { Helmet } from "react-helmet-async";
 
 const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state?.from?.pathname || "/";
-  
+
   const {
     register,
     handleSubmit,
@@ -49,71 +49,64 @@ const Login = () => {
   };
 
   return (
-    <>
+    <div>
       <Helmet>
-        <title>Bistro Boss | Login</title>
+        <title>Hello World</title>
       </Helmet>
-      <div>
-        <div className="flex justify-center items-center my-8">
-          <img className="w-4/12 m-0" src={login} alt="" />
-          <div className="card w-96 bg-slate-200 shadow-xl items-center text-center py-8 rounded-lg">
-            <Form
-              onSubmit={handleSubmit(onSubmit)}
-            >
-              <h1 className="text-2xl font-bold">LogIn</h1>
-              <div className="py-6 w-full">
-                <input
-                  type="email"
-                  placeholder="Email"
-                  {...register("email")}
-                  required
-                  className="input w-full max-w-xs"
-                />
-              </div>
-              <div className="w-full pb-2">
-                <input
-                  type="password"
-                  placeholder="Password"
-                  {...register("password")}
-                  required
-                  className="input w-full max-w-xs"
-                />
-              </div>
+      <div className="flex justify-center items-center my-8">
+        <img className="w-4/12 m-0" src={login} alt="" />
+        <div className="card w-96 bg-slate-200 shadow-xl items-center text-center py-8 rounded-lg">
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <h1 className="text-2xl font-bold">LogIn</h1>
+            <div className="py-6 w-full">
+              <input
+                type="email"
+                placeholder="Email"
+                {...register("email")}
+                required
+                className="input w-full max-w-xs"
+              />
+            </div>
+            <div className="w-full pb-2">
+              <input
+                type="password"
+                placeholder="Password"
+                {...register("password")}
+                required
+                className="input w-full max-w-xs"
+              />
+            </div>
 
-              <p className="text-start text-base font-semibold">
-                create a new acount
-                <Link
-                  to="/signup"
-                  className="text-yellow-400-700 underline pl-2"
-                >
-                  Register
-                </Link>
-              </p>
-              <button type="submit" className="btns-primary">
-                LogIn
-              </button>
-            </Form>
-            <div className="py-4">
-                <button
-                  onClick={handleGoogleLogIn}
-                  className="text-base flex items-center gap-3 font-semibold btns-primary"
-                >
-                  <span>
-                    <FaGoogle />
-                  </span>{" "}
-                  Continue with google
-                </button>
-                <button className="text-base flex items-center gap-3 font-semibold btns-primary">
-                  <span>
-                    <FaGithub />
-                  </span>{" "}
-                  Continue with github
-                </button>
-              </div>
+            <p className="text-start text-base font-semibold">
+              create a new acount
+              <Link to="/signup" className="text-yellow-400-700 underline pl-2">
+                Register
+              </Link>
+            </p>
+            <button type="submit" className="btns-primary">
+              LogIn
+            </button>
+          </Form>
+          <div className="py-4">
+            <button
+              onClick={handleGoogleLogIn}
+              className="text-base flex items-center gap-3 font-semibold btns-primary"
+            >
+              <span>
+                <FaGoogle />
+              </span>{" "}
+              Continue with google
+            </button>
+            <button className="text-base flex items-center gap-3 font-semibold btns-primary">
+              <span>
+                <FaGithub />
+              </span>{" "}
+              Continue with github
+            </button>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
