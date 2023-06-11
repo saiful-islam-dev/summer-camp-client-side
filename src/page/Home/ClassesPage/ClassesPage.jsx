1c54547862bbabd9390775ec68d3b782b79d0fcb
Saiful264 <saifulislamx264@gmail.com>
2023-06-11 11:13:29 +0600

    github problem slove

--- src/page/Home/ClassesPage/ClassesPage.jsx
+++ src/page/Home/ClassesPage/ClassesPage.jsx
@@ -4,7 +4,7 @@ import img from "../../../assets/home/profile.jpg";
 const ClassesPage = () => {
   return (
     <div>
-      <SectionTitle heading="our classes" subHeading=''/>
+      <SectionTitle heading="our classes" subHeading='Join us for an exciting sports camp adventure that combines skill development, team building, and friendly competition. From honing your techniques to exploring various sports, our camp offers a dynamic and inclusive experience for young athletes to thrive, grow, and foster a love for sports.'/>
 
       <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
         <div className="card w-96 border-4 border-slate-900 rounded-md bg-base-300 shadow-xl">
@@ -17,10 +17,10 @@ const ClassesPage = () => {
             <div className="flex justify-between">
               <div>
               <p className="capitalize">
-                  <span className="font-semibold">Name: </span> saifuol
+                  <span className="font-semibold">Name:</span> saifuol
                 </p>
                 <p className="capitalize">
-                  <span className="font-semibold">Instructor name: </span> saifuol
+                  <span className="font-semibold">Instructor:</span> saifuol
                 </p>
               </div>
               <div>
@@ -32,7 +32,7 @@ const ClassesPage = () => {
                 </p>
               </div>
             </div>
-              <button className="btn btn-success">Select</button>
+              <button className="btn btn-active btn-ghost">Select</button>
           </div>
         </div>
       </div>
